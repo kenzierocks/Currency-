cebfb1cb5240cee4a77470530186da0d2a77ca13
Kenzie Togami <ket1999@gmail.com>
2016-01-02 10:08:09 -0800

    lie(cense)

--- src/main/java/me/kenzierocks/plugins/currencysnowmen/implementation/DataMapAdapter.java
+++ src/main/java/me/kenzierocks/plugins/currencysnowmen/implementation/DataMapAdapter.java
@@ -1,3 +1,27 @@
+/*
+ * This file is part of Currency☃, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) kenzierocks (Kenzie Togami) <http://kenzierocks.me>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package me.kenzierocks.plugins.currencysnowmen.implementation;
 
 import java.lang.reflect.Type;
@@ -21,6 +45,7 @@ import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Table;
 import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.InstanceCreator;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
@@ -77,7 +102,8 @@ final class DataMapAdapter
 
     }
 
-    private static final Gson NORMAL_JSON = new Gson();
+    private static final Gson NORMAL_JSON =
+            new GsonBuilder().enableComplexMapKeySerialization().create();
     @SuppressWarnings("serial")
     private static final Type DATA_TYPE =
             new TypeToken<Map<String, Map<Map<String, String>, BigDecimal>>>() {
@@ -92,6 +118,7 @@ final class DataMapAdapter
     public Table<Currency, Set<Context>, BigDecimal> deserialize(
             JsonElement json, Type typeOfT, JsonDeserializationContext context)
                     throws JsonParseException {
+        System.err.println(json);
         Map<String, Map<Map<String, String>, BigDecimal>> data =
                 NORMAL_JSON.fromJson(json, DATA_TYPE);
         Table<Currency, Set<Context>, BigDecimal> target =
@@ -109,18 +136,18 @@ final class DataMapAdapter
     @Override
     public JsonElement serialize(Table<Currency, Set<Context>, BigDecimal> src,
             Type typeOfSrc, JsonSerializationContext context) {
-        Table<String, Map<String, String>, BigDecimal> tableData =
-                HashBasedTable.create();
+        Map<String, Map<Map<String, String>, BigDecimal>> data =
+                new HashMap<>();
         src.rowMap().forEach((currency, contextToAmount) -> {
             String id = ((ExtendedCurrency) currency).getIdentifer();
             contextToAmount.forEach((contextSet, amount) -> {
-                tableData.put(id,
-                        contextSet.stream().collect(Collectors
+                data.computeIfAbsent(id, k -> new HashMap<>())
+                        .put(contextSet.stream().collect(Collectors
                                 .toMap(Entry::getKey, Entry::getValue)),
                         amount);
             });
         });
-        return NORMAL_JSON.toJsonTree(tableData.rowMap(), DATA_TYPE);
+        return NORMAL_JSON.toJsonTree(data, DATA_TYPE);
     }
 
 }
